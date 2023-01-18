81ddfaacc91ad9fce630dde13b21985adfb74544
Nico-Stern <120172693+Nico-Stern@users.noreply.github.com>
2023-01-18 13:44:46 +0100

    design 18.1

--- Assets/Scripts/Kristalle/Item.cs
+++ Assets/Scripts/Kristalle/Item.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,12 +6,21 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public Itemzaehler Kristall;
+    public int Kzahl = 0;
+    public GameObject NK;
+
+    private void Start()
+    {
+        if(Kzahl !=1)
+        gameObject.SetActive(false);
+    }
 
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player1") || (collision.gameObject.CompareTag("Player2")))
             gameObject.SetActive(false);
             Kristall.Kristalle += 1;
+            NK.SetActive(true);
     }
 }
 
