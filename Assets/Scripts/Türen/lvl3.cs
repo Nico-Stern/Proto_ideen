eeb626a961fd94152927fc3ac00812e3cb1c2e1a
Nico-Stern <nstern2002@gmail.com>
2023-01-20 23:08:42 +0100

    eee

---
+++ Assets/Scripts/Türen/lvl3.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class lvl3 : MonoBehaviour
+{
+    public Itemzaehler zaehler;
+    public Ziel ziel;
+    public int Kristalle;
+    public int Ziel;
+    void Start()
+    {
+        gameObject.SetActive(false);
+        Kristalle = zaehler.Kristalle;
+        Ziel = ziel.Zielkristale;
+    }
+
+    public void Update()
+    {
+        Kristalle = zaehler.Kristalle;
+        Ziel = ziel.Zielkristale;
+
+
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player1") || (collision.gameObject.CompareTag("Player2")))
+        {
+            SceneManager.LoadScene("Lvl.3");
+        }
+    }
+
+}
