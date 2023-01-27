f860974bc36e365150cd32e0cc5bce13fd688930
Nico-Stern <120172693+Nico-Stern@users.noreply.github.com>
2023-01-27 13:36:00 +0100

    test

--- Assets/Scripts/DruckNormal.cs
+++ Assets/Scripts/DruckNormal.cs
@@ -5,21 +5,19 @@ using UnityEngine;
 
 public class DruckNormal : MonoBehaviour
 {
+    private AudioSource Source;
+    public AudioClip sound;
     public int Triggernormal = 0;
-    void Update()
-    {
-        
-    }
-
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Emmy") || (collision.gameObject.CompareTag("Toto") || collision.gameObject.CompareTag("Kiste")))
         {           
             Triggernormal += 1;
             print(Triggernormal);
+            Source.clip = sound;
+            Source.Play();
         }
     }
-
     public void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Emmy") || (collision.gameObject.CompareTag("Toto") || collision.gameObject.CompareTag("Kiste") ))
